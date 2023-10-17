2b059a2d912d74d140fd20d903d2b12fff352701
Hakan Kaya <hakaya@outlook.com>
2023-10-17 14:14:05 +0300

    Initial Commit

---
+++ ToDoOOP/ToDoOOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoOOPApp.swift
+//  ToDoOOP
+//
+//  Created by Hakan Kaya on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoOOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
